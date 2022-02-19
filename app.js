bf48f676f6f7a722480654c3c5f4e437f430e21c
Judy <judy.mtz1@gmail.com>
2022-02-19 14:14:11 -0600

    added profile questions

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const promptUser = () => {
       type: 'input',
       name: 'name',
       message: 'What is your name?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username'
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
     }
   ]);
 };
